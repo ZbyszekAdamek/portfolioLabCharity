e08df6c9134a365e684bc4a5802426b53ffa9de2
ZbyszekAdamek <zbigniew.t.adamek@gmail.com>
2021-11-14 10:49:22 +0100

    Dodanie akcji home - wyświetlanie liczby oddanych darów

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -3,7 +3,7 @@ package pl.coderslab.charity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.coderslab.charity.repository.InstitutionRepository;
+import pl.coderslab.charity.service.JpaDonationService;
 import pl.coderslab.charity.service.JpaInstitutionService;
 
 
@@ -11,14 +11,17 @@ import pl.coderslab.charity.service.JpaInstitutionService;
 public class HomeController {
 
 private final JpaInstitutionService jpaInstitutionService;
+private final JpaDonationService jpaDonationService;
 
-    public HomeController(JpaInstitutionService jpaInstitutionService) {
+    public HomeController(JpaInstitutionService jpaInstitutionService, JpaDonationService jpaDonationService) {
         this.jpaInstitutionService = jpaInstitutionService;
+        this.jpaDonationService = jpaDonationService;
     }
 
     @RequestMapping("/")
     public String homeAction(Model model){
         model.addAttribute("institutions", jpaInstitutionService.getAllInstitutions());
+        model.addAttribute("donations", jpaDonationService.getAllDonations());
         return "index";
     }
 
---
+++ src/main/java/pl/coderslab/charity/service/JpaDonationService.java
@@ -0,0 +1,21 @@
+package pl.coderslab.charity.service;
+
+import org.springframework.stereotype.Service;
+import pl.coderslab.charity.entity.Donation;
+import pl.coderslab.charity.repository.DonationRepository;
+
+import java.util.List;
+
+@Service
+public class JpaDonationService implements DonationService {
+    private final DonationRepository donationRepository;
+
+    public JpaDonationService(DonationRepository donationRepository) {
+        this.donationRepository = donationRepository;
+    }
+
+    @Override
+    public List<Donation> getAllDonations() {
+        return donationRepository.findAll();
+    }
+}
