d0d49772e7962545a152127ce0ce6e1e2c1e907f
ZbyszekAdamek <zbigniew.t.adamek@gmail.com>
2021-11-20 20:09:49 +0100

    Możliwość dodania datku na wybraną organizację.

--- src/main/webapp/resources/js/form.js
+++ src/main/webapp/resources/js/form.js
@@ -1,27 +1,33 @@
 function finalizingForm() {
 
+    //zmienna quantityOfBags tworzona na podstawie elementów dokumentu zebranych poprzez id
+    //quantityOfBags
+
     let quantityOfBags = document.getElementById("quantityOfBags");
+
     if (quantityOfBags.value === '1') {
         document.getElementById("finalQuantity").innerText = quantityOfBags.value + ' worek darów';
-    }else if (quantityOfBags.value === '2'
-        || quantityOfBags.value === '3'
-        || quantityOfBags.value === '4'
+    }else if (quantityOfBags.value === '2' || quantityOfBags.value === '3' || quantityOfBags.value === '4'
     ){
         document.getElementById("finalQuantity").innerText = quantityOfBags.value + ' worki darów';
     } else {
         document.getElementById("finalQuantity").innerText = quantityOfBags.value + ' worków darów';
     }
 
+    //zmienna tworzona na podstawie wybrania z dokumentu elementów input name institution
+
     let institutions = document.querySelectorAll('input[name="institution"]');
-    let selected;
+    let selected = 0;
     for (const item of institutions){
         if (item.checked){
             selected = item.value;
             break;
         }
     }
-    let selectedInstitution = document.getElementById("selected" + selected);
-    document.getElementById("institutionName").innerText = selectedInstitution.innerText;
+    //zmienne określające poszczególne elementy ankiety i ich wartości value
+
+    let chosenInstitution = document.getElementById("chosen" + selected);
+    document.getElementById("institutionName").innerText = chosenInstitution.innerText;
 
     let street = document.getElementById("formStreet");
     document.getElementById("liStreet").innerText = street.value;
@@ -35,17 +41,16 @@ function finalizingForm() {
     let phone = document.getElementById("formPhone");
     document.getElementById("liPhone").innerText = phone.value;
 
-    let data = document.getElementById("formDate");
-    document.getElementById("liData").innerText = data.value;
+    let date = document.getElementById("formDate");
+    document.getElementById("liData").innerText = date.value;
 
     let time = document.getElementById("formTime");
     document.getElementById("liTime").innerText = time.value;
 
-    let comment = document.getElementById("formPickUpComment");
-    if (comment.value === ''){
+    let pickUpComment = document.getElementById("formPickUpComment");
+    if (pickUpComment.value === ''){
         document.getElementById('liComment').innerText = 'Brak uwag'
     } else {
-        document.getElementById("liComment").innerText = comment.value;
+        document.getElementById("liComment").innerText = pickUpComment.value;
     }
-
 }
