b37d66bb59e26184ed69eb2ec6a6458aff704a1a
ZbyszekAdamek <zbigniew.t.adamek@gmail.com>
2021-12-14 00:35:24 +0100

    Properties dostosowane do heroku

--- src/main/java/pl/coderslab/charity/AppConfig.java
+++ src/main/java/pl/coderslab/charity/AppConfig.java
@@ -1,20 +1,20 @@
 package pl.coderslab.charity;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ConversionServiceFactoryBean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-
-import javax.validation.Validator;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
@@ -22,7 +22,15 @@ import java.util.Set;
 @Configuration
 @ComponentScan("pl.coderslab")
 @EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "pl.coderslab")
 public class AppConfig {
+
+    @Bean(name="localeResolver")
+    public LocaleContextResolver getLocaleContextResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pl","PL"));
+        return localeResolver;
+    }
 
     @Bean
     public MessageSource messageSource() {
@@ -31,25 +39,20 @@ public class AppConfig {
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
+
     @Bean
     public Validator validator() {
         LocalValidatorFactoryBean lvfb = new LocalValidatorFactoryBean();
         lvfb.setValidationMessageSource(messageSource());
         return lvfb;
     }
-    @Bean(name="localeResolver")
-    public LocaleContextResolver getLocaleContextResolver() {
-        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-        localeResolver.setDefaultLocale(new Locale("pl","PL"));
-        return localeResolver;
+
+
+    @Bean(name = "conversionService")
+    public ConversionService getConversionService() {
+        ConversionServiceFactoryBean factory = new ConversionServiceFactoryBean();
+        factory.afterPropertiesSet();
+        return factory.getObject();
     }
-    @Bean
-    public FilterRegistrationBean filterRegistrationBean() {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-        characterEncodingFilter.setForceEncoding(true);
-        characterEncodingFilter.setEncoding("UTF-8");
-        registrationBean.setFilter(characterEncodingFilter);
-        return registrationBean;
-    }
+
 }
